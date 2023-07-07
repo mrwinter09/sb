299b29810556361c60d964465bda10a45cdd4579
mrwinter09 <ivan.winter09@gmail.com>
2023-07-07 15:47:44 +0200

    cheeck

--- src/component/BlogPosts/BlogPosts.js
+++ src/component/BlogPosts/BlogPosts.js
@@ -1,13 +1,13 @@
 /** @format */
 
 import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import ReactPaginate from 'react-paginate'
 import axios from 'axios'
 import './BlogPosts.css'
 
 const BlogPosts = () => {
-  const url = 'https://api.coinstats.app/public/v1/coins?skip=0&limit=8'
-  const [coins, setCoins] = useState(null)
+  const url = 'https://api.coinstats.app/public/v1/coins?skip=0&limit=100'
+  const [coins, setCoins] = useState([])
 
   useEffect(() => {
     axios
@@ -21,15 +21,33 @@ const BlogPosts = () => {
       })
   }, [])
 
-  if (!coins) return null
+  const [currentItems, setCurrentItems] = useState([])
+  const [pageCount, setPageCount] = useState(0)
+  const [itemOffset, setItemOffset] = useState(0)
 
-  console.log(coins)
+  const itemsPerPage = 8
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage
+    console.log(`Loading items from ${itemOffset} to ${endOffset}`)
+    setCurrentItems(coins.slice(itemOffset, endOffset))
+    setPageCount(Math.ceil(coins.length / itemsPerPage))
+  }, [itemOffset, coins])
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % coins.length
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    )
+    setItemOffset(newOffset)
+  }
+
   return (
     <>
       <div className="cms cms-blog-page">
         <div className="blog-page-container">
           <div className="blog-page-cards">
-            {coins.map((coins) => {
+            {currentItems.map((coins) => {
               return (
                 <div key={coins.rank} className="blog-card">
                   <div className="top">
@@ -48,14 +66,20 @@ const BlogPosts = () => {
             })}
           </div>
           <div class="sb-blog-page-container">
-            <Link to="/blogpage">
-              <button
-                type="submit"
-                class="btn btn-primary"
-                id="sb-blog-page-button">
-                Laad meer
-              </button>
-            </Link>
+            <ReactPaginate
+              breakLabel="..."
+              nextLabel="Volgende pagina >"
+              onPageChange={handlePageClick}
+              pageRangeDisplayed={5}
+              pageCount={pageCount}
+              previousLabel="< previous"
+              renderOnZeroPageCount={null}
+              containerClassName="pagination"
+              pageLinkClassName="page-num"
+              previousLinkClassName="page-num"
+              nextLinkClassName="page-num"
+              activeClassName="active"
+            />
           </div>
         </div>
       </div>
