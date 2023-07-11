6b44e37fc29f9bf12a7e84b5f39ebc0280497ee2
mrwinter09 <ivan.winter09@gmail.com>
2023-07-11 15:51:34 +0200

    check

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 /** @format */
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage'
 import BlogPage from './pages/BlogPage/BlogPage'
 import NavBar from './component/NavBar/NavBar'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
